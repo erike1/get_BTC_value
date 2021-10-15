5b486d7039e4579f53f01ba1d2fdbe434645e190
Erike Allan <erikeahennich@gmail.com>
2021-10-15 20:37:45 -0300

    depois de ter dado certo o push in a existing repository with POWERSHELL7, now Trying to push with vs code terminal

---
+++ app.py
@@ -0,0 +1,9 @@
+# pelo PWERSHELL7 funciona fazer o push para o github. 
+"""
+se você já criou o repositório no gitHUB segue esses passos PORRA:
+nessa exata ordem:  git@eOcaralho é o link SSH
+git remote add origin git@github.com:erike1/monitor-de-precos.git
+git branch -M main
+git push -u origin main
+
+"""
